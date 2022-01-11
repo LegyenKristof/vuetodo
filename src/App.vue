a957e741d9d56fc7ff2a0dcf2c4493dee738a02b
Your Name <you@example.com>
2022-01-11 17:25:30 +0100

    kesz

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,99 @@
 <template>
   <div id="app">
-    <Todo :todos="todos" @todo-item-changed="Changed"/>
+    <table>
+      <thead>
+        <th>Id</th>
+        <th>Title</th>
+        <th>Year</th>
+        <th>On Display</th>
+      </thead>
+      <tbody>
+        <tr v-for="painting in paintings" :key="painting.id">
+          <td>{{painting.id}}</td>
+          <td>{{painting.title}}</td>
+          <td>{{painting.year}}</td>
+          <td>{{painting.on_display}}</td>
+          <td><button @click="deletePainting(painting.id)">Delete</button></td>
+        </tr>
+        <tr>
+          <td><input type="hidden" v-model="painting.id"></td>
+          <td><input type="text" v-model="painting.title"></td>
+          <td><input type="number" v-model="painting.year"></td>
+          <td><input type="checkbox" v-model="painting.on_display"></td>
+          <td><button @click="addPainting" :disabled="saving">Add</button></td>
+        </tr>
+      </tbody>
+    </table>
+    <button @click="loadData">Load</button>
   </div>
 </template>
 
 <script>
-import Todo from './components/Todo.vue'
 
 export default {
   name: 'App',
   components: {
-    Todo
   },
   data() {
     return {
-      todos: [
-        {
-          title: 'Első teendő',
-          valami: 'valami'
-        },
-        {
-          title: 'Második teendő'
-        },
-        {
-          title: 'Harmadik teendő'
-        },
-        ]
+      paintings: [],
+      painting: {
+        id: null,
+        title: null,
+        year: null,
+        on_display: false
+      },
+      saving: false
     }
   },
   methods: {
-    Changed(e) {
-      this.todos.map(function (todo) {
-        if (todo.title != e.original.title) {
-          return todo
-        }
-        todo.title = e.new.title
-        return todo
+    async loadData(){
+
+      let response = await fetch("http://127.0.0.1:8000/api/paintings")
+      let data = await response.json()
+
+      this.paintings = data
+      console.log(this.paintings)
+    },
+
+    async deletePainting(id){
+      await fetch(`http://127.0.0.1:8000/api/paintings/${id}`, {
+        method: "DELETE"
       })
-    }
+
+      await this.loadData()
+    },
+
+    async addPainting(){
+      this.saving = true
+      await fetch(`http://127.0.0.1:8000/api/paintings`, {
+        method: "POST",
+        headers: {
+          "Content-type" : "application/json",
+          "Accept" : "application/json"
+        },
+        body: JSON.stringify(this.painting)
+      })
+
+      await this.loadData()
+      this.saving = false
+    },
+
+    async updatePainting(id){
+      await fetch(`http://127.0.0.1:8000/api/paintings/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-type" : "application/json",
+          "Accept" : "application/json"
+        },
+        body: JSON.stringify(this.painting)
+      })
+
+      await this.loadData()
+    },
+  },
+  mounted() {
+    this.loadData()
   }
 }
 </script>
